53039f5022341c9f8b194f172a0be54017bc3d96
gwansikk <seorit98@gmail.com>
2022-09-29 12:07:54 +0900

    [A] 객체프 코딩시험

---
+++ DD015_1251/CT1/src/Contact.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Contact {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<People> peopleList = new ArrayList<People>();
+
+    void run() {
+        readAll();
+
+        int kwd = 0;
+
+        while (true) {
+            System.out.println("############ 연락처 관리 ############");
+            System.out.print("(1) 연락처 출력  (2) 새로 등록  (3) 삭제  (4) 검색  (5) 수정  (6) 끝내기  ... ");
+            kwd = scanner.nextInt();
+
+            switch (kwd) {
+                case 1:
+                    printAll();
+                    break;
+                case 2:
+                    addPeople();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    search();
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    return;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    void addPeople() {
+        String phone = null;
+        String name = null;
+        String ord = null;
+
+        System.out.print("전화번호 : ");
+        phone = scanner.next();
+        System.out.print("이름 : ");
+        name = scanner.next();
+        System.out.print("나이 : ");
+        ord = scanner.next();
+
+        People pe = new People(phone, name, ord);
+        peopleList.add(pe);
+    }
+
+    void search() {
+        String kwd = null;
+
+        System.out.print("검색할 키워드: ");
+        kwd = scanner.next();
+
+        for (int i = 0; i < peopleList.size(); i++) {
+            if (peopleList.get(i).matchs(kwd)) {
+                System.out.printf("[%d] ", i + 1);
+                peopleList.get(i).print();
+            }
+        }
+    }
+
+    void printAll() {
+        int count = 1;
+
+        for (People people : peopleList) {
+            System.out.printf("[%d] ", count++);
+            people.print();
+        }
+    }
+
+    void readAll() {
+        String kwd = null;
+
+        while (true) {
+            kwd = scanner.next();
+
+            if (kwd.equals("end"))
+                break;
+
+            People pe = new People(kwd);
+            pe.read(scanner);
+
+            peopleList.add(pe);
+        }
+    }
+
+    public static void main(String[] args) {
+        Contact contact = new Contact();
+        contact.run();
+    }
+}
---
+++ DD015_1251/CT1/src/People.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class People {
+    String phone;
+    String name;
+    String ord;
+
+    People(String phone) {
+        this.phone = phone;
+    }
+
+    People(String phone, String name, String ord) {
+        this(phone);
+        this.name = name;
+        this.ord = ord;
+    }
+
+    void read(Scanner scanner) {
+        name = scanner.next();
+        ord = scanner.next();
+    }
+
+    Boolean matchs(String kwd) {
+        if (phone.contains(kwd))
+            return true;
+
+        if (name.contains(kwd))
+            return true;
+
+        if (ord.equals(kwd))
+            return true;
+
+        return false;
+    }
+
+    public void print() {
+        System.out.printf("%-20s %3s세 %-14s\n", name, ord, phone);
+    }
+}
